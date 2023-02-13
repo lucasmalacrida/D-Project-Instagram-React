32adf71d96428ab6b73d670f657e40992677a5a7
lucasmalacrida <lucasmd99@hotmail.com>
2023-02-13 17:53:39 -0300

    Implementada Edição de Nome e Foto do Usuário

--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 export default function Usuario() {
+    const [profileName,setProfileName] = useState("");
+    const [profilePic,setProfilePic] = useState("");
+
+    const initialProfileName = "catanacomics";
+    const initialProfilePic = "../../assets/pics/catana.jpeg";
+
+    function loadProfileName(){
+        setProfileName(prompt("Digite seu novo nome:"));
+    }
+
+    function loadProfilePic(){
+        setProfilePic(prompt("Digite a URL da sua nova foto:"));
+    }
+
     return (
         <div class="logged-account">
-            <img class="pic-big" src="../../assets/pics/catana.jpeg" onClick={()=>(prompt("Digite a URL da sua nova foto:"))} data-test="profile-image" />
+            <img class="pic-big" src={profilePic? profilePic : initialProfilePic} onClick={() => loadProfilePic()} data-test="profile-image" />
             <div class="profile">
-                <h1 data-test="name">catanacomics</h1>
-                <ion-icon name="pencil" onClick={()=>(prompt("Digite seu novo nome:"))} data-test="edit-name"></ion-icon>
+                <h1 data-test="name">{profileName? profileName : initialProfileName}</h1>
+                <ion-icon name="pencil" onClick={() => loadProfileName()} data-test="edit-name"></ion-icon>
             </div>
         </div>
     )
